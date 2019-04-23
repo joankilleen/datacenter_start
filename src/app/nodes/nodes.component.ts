cf99f16a6e469f4ef354bf0390441e7858664542
Joan Killeen <joan.killeen@bluewin.ch>
2019-04-23 14:04:28 +0200

    Chapter 4

---
+++ src/app/nodes/nodes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: '[app-nodes¨]',
+  templateUrl: './nodes.component.html',
+  styleUrls: ['./nodes.component.css']
+})
+export class NodesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
