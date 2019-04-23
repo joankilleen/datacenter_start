d0b6a92f96d3def69cee9ef86212a330886d40c6
Joan Killeen <joan.killeen@bluewin.ch>
2019-04-23 15:10:07 +0200

    Chapter 5 Events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';     
+import { DashboardComponent } from './dashboard/dashboard.component';     
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  @ViewChild(DashboardComponent) dashboard: DashboardComponent;     
+  refresh() {     
+	this.dashboard.generateData();     
+  }     
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Output, EventEmitter } from '@angular/core';     
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class NavbarComponent  {
+  @Output() onRefresh: EventEmitter<null> = new EventEmitter<null>();     
+  refresh() {     
+	this.onRefresh.emit();     
+  }     
 }
