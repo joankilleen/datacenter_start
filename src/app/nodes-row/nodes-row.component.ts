fec065fd4d1c59e8f6d4c36ab2aab5a9bafb032e
Joan Killeen <joan.killeen@bluewin.ch>
2019-04-26 11:49:38 +0200

    ng-bootstrap modal

--- src/app/nodes-row/nodes-row.component.ts
+++ src/app/nodes-row/nodes-row.component.ts
@@ -1,13 +1,22 @@
-import { Component, Input, ChangeDetectionStrategy } from '@angular/core';     
+import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';     
+import { NodesDetailComponent } from '../nodes-detail/nodes-detail.component';     
 @Component({
   selector: '[app-nodes-row]',
   templateUrl: './nodes-row.component.html',
   styleUrls: ['./nodes-row.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush     
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NodesRowComponent {
-  @Input() node: any;     
-  isDanger(prop) {     
-    return this.node[prop].used / this.node[prop].available > 0.7;     
-  }     
+  @Input() node: any;
+
+  constructor(private modalService: NgbModal) {}     
+  isDanger(prop) {
+    return this.node[prop].used / this.node[prop].available > 0.7;
+  }
+
+  open(node) {     
+    const modal = this.modalService.open(NodesDetailComponent);     
+    modal.componentInstance.node = node;     
+  }     
 }
